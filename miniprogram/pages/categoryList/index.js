4f6d27ebd54a7b194e5405be3ceb5e0e51119c80
zoneTian <Tian.Zhao@connext.com.cn>
2019-03-11 18:16:00 +0800

    update

--- miniprogram/pages/categoryList/index.js
+++ miniprogram/pages/categoryList/index.js
@@ -1,12 +1,8 @@
-//.js
+//categorylist index.js
 const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
     details: [],
     scrollHeight:""
   },
@@ -17,51 +13,18 @@ Page({
       url:"http://rap2api.taobao.org/app/mock/25013/user/backlists",
       method:"get",
       success:(data)=>{
-        console.log(data.data.results)
         _this.setData({
           details : data.data.results.details
         })
-
-        
       }
     })
-    // if (!wx.cloud) {
-    //   wx.redirectTo({
-    //     url: '../chooseLib/chooseLib',
-    //   })
-    //   return
-    // }
     wx.getSystemInfo({
       success(res){
         _this.setData({
           scrollHeight: res.windowHeight - 20 +'px'
         })
-        // onsole.log(res.windowHeight)
-      }
-
-    })
-    wx.getUserInfo({
-      success:res => {
-        console.log(res)
       }
     })
-    // 获取用户信息
-    // wx.getSettig({
-    //   success: res => {
-    //     console.log(res)
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           this.setData({
-    //             avatarUrl: res.userInfo.avatarUrl,
-    //             userInfo: res.userInfo
-    //           })
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
   },
   onShareAppMessage:()=>{
     return {
@@ -69,22 +32,16 @@ Page({
     }
   },
   onbottom:function (){
-    console.log(234)
     let _this = this
     wx.request({
       url:"http://rap2api.taobao.org/app/mock/25013/user/backlists",
       method:"get",
       success:(data)=>{
-        // this.details = this.
-        // var a = this.details
          let datas = this.data.details.concat(data.data.results.details)
          _this.setData({
            details : datas
          })
-        console.log(this.data)
       }
     })
   },
- 
-
 })
--- miniprogram/pages/getImage/index.js
+++ miniprogram/pages/getImage/index.js
@@ -1,5 +1,6 @@
-//index.js
+//getimage index.js
 const app = getApp()
+const regeneratorRuntime = require('../../util/runtime')
 
 Page({
   data: {
@@ -7,16 +8,14 @@ Page({
     userInfo: {},
     logged: false,
     takeSession: false,
+    imgpath:"",
+    show:true
 	},
   onLoad: function() {
-
-
-
 	
   },
 
   onGetUserInfo: function(e) {
-    console.log(e)
     if (!this.logged && e.detail.userInfo) {
       this.setData({
         logged: true,
@@ -25,24 +24,144 @@ Page({
       })
     }
   },
-  onCanvas:function(e){
-    // console.log(this.onGetUserInfo(e))
-    console.log(e)
+  procanvasToTempFilePath:()=>{
+    return new Promise((res,rej)=>{
+      wx.canvasToTempFilePath({
+        canvasId:"card",
+        quality:1,
+        success:res,
+        fail:rej
+      })
+    })
+  },
+  saveImage(rs){
+    let that = this 
+    return new Promise((res,rej)=>{
+      wx.saveImageToPhotosAlbum({
+        filePath:that.data.imgpath,
+        success:res,
+        fail:rej
+      })
+    })
+  },
+  downloadA(url){
+    return new Promise((res,rej)=>{
+      wx.downloadFile({
+        url:url,
+        success:res,
+        fail:rej
+      }) 
+    })
+  },
+  opensetting(res){
+   let that = this
+   return new Promise((res,rej)=>{
+     wx.openSetting({
+       success(data){
+         if(data.authSetting['scope.writePhotosAblum']){
+           res(data)
+         }else{
+          that.saveImage()
+         }
+       },
+       fail(err){
+         rej(err)
+       }
+     })
+   })
+  },
+  showmodal(){
+    let that = this
+    return  new Promise((res,rej)=>{
+      wx.showModal(
+        {
+          content:"请授权",
+          success(data){
+            if(data.confirm){
+              that.opensetting().then(()=>{
+                res()
+              })
+            }else{
+              // that.saveImage(res)
+            }
+          }
+        }
+      )
+    })
+  },
+  async onCanvas(e){
+    let that = this
     const ctx=wx.createCanvasContext('card')
     ctx.setTextAlign("center");
     ctx.setFillStyle("#000000");
     ctx.setFontSize(16);
     ctx.fillText(e.detail.userInfo.nickName,100,30)
     const size = 80;
-    ctx.drawImage(e.detail.userInfo.avatarUrl,60,50,size,size)
+    let res = await this.downloadA(e.detail.userInfo.avatarUrl);
+    ctx.drawImage(res.tempFilePath,60,50,size,size)
     ctx.stroke();
-    ctx.draw()
-		// wx.login({
-		// 	success(res){
-		// 		console.log("login",res)
-		// 	}
+    ctx.draw(false,async ()=>{
+      let res = await this.procanvasToTempFilePath()
+      that.setData({
+        imgpath:res.tempFilePath,
+        show:false
+      })
+      try{
+        await this.saveImage(res)
+        wx.showToast({
+          title:"图片生成成功",
+          icon:"none"
+        })
+      }catch(e){
+        if(e.errMsg === 'saveImageToPhotosAlbum:fail:auth denied' ||
+        e.errMsg === 'saveImageToPhotosAlbum:fail auth deny') {
+          await that.showmodal()
+        }
 
-		// })
+        // wx.sshowToast({
+        //   title:"图片生成失败",
+        //   icon:"none"
+        // })
+       
+        // wx.setting
+      }
+    })
+    // wx.downloadFile({
+    //   url:e.detail.userInfo.avatarUrl,
+    //   success:(res)=>{
+    //     // ctx.drawImage(res.tempFilePath,60,50,size,size)
+    //     // ctx.stroke();
+    //     ctx.draw(false,()=>{
+    //       wx.canvasToTempFilePath({
+    //         canvasId:"card",
+    //         quality:1,
+    //         success(rs){
+    //           that.setData({
+    //             imgpath:rs.tempFilePath,
+    //             show:false
+    //           })
+    //           wx.saveImageToPhotosAlbum({
+    //             filePath:rs.tempFilePath,
+    //             success(mes){
+    //               wx.showToast({
+    //                 title:"图片生成成功",
+    //                 icon:"none"
+    //               })
+    //             },
+    //             fail(res){
+    //               wx.showToast({
+    //                 title:"图片生成失败",
+    //                 icon:"none"
+    //               })
+    //             }
+    //           }) 
+        
+    //         }
+    //       })
+         
+    //     })
+    //     }
+    // })
 	},
   onGetOpenid: function() {
     // 调用云函数
@@ -50,70 +169,22 @@ Page({
       name: 'login',
       data: {},
       success: res => {
-        console.log('[云函数] [login] user openid: ', res.result)
-				app.globalData.openid = res.result.openid
-				
+				app.globalData.openid = res.result.openid		
         wx.navigateTo({
           url: '../userConsole/userConsole',
         })
       },
       fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
         wx.navigateTo({
           url: '../deployFunctions/deployFunctions',
         })
       }
     })
   },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
+  onShareAppMessage:function(opt){
+    return {
+      title:"转发我的名片",
+    }
   },
 
 })
